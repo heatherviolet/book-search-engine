1f18569a6bd2c3cf14d8583cb1f7582930a46912
Heather Violet <heatherviolet91@gmail.com>
2022-04-10 19:58:43 -0700

    updated resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -6,21 +6,21 @@ const resolvers = {
             return Book.find().sort({ createdAt: -1 });
         },
         book: async (parent, { _id }) => {
-            return Thought.findOne({ _id });
+            return Book.findOne({ _id });
         },
         // get all users
         users: async () => {
             return User.find()
                 .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
+                .populate('books')
+                .populate('savedBooks');
         },
         // get a user by username
         user: async (parent, { username }) => {
             return User.findOne({ username })
                 .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
+                .populate('books')
+                .populate('savedBooks');
         },
     }
 };
